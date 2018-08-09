6ddf29f49a22016b2fd6edc9f9e25da6d14e9151
Frogkiller <frogkillerpl@gmail.com>
2018-08-09 20:05:29 +0200

    initial split of main to backend (calculation) and prices (const price values)

---
+++ backend.py
@@ -0,0 +1,59 @@
+def ceiling_half(value):
+    rv = round(value)
+    if value - rv < 0:
+        return rv
+    else:
+        return rv + 0.5
+
+
+def mats_price(size, tkan, wata):
+    price = size * PRICE_baw
+    price += size * tkan
+    if wata == 1:
+        price += size * PRICE_wat
+    price += size * value_waste
+    return ceiling_half(price)
+
+
+def kostka(szer, dl, wys, tkan, wata):
+    size = (szer*dl + szer*wys + wys*dl)*2
+    price = mats_price(size, tkan, wata)
+    if size < 2600:
+        price += size * value_kostka_small
+    elif size <3700:
+        price += size * value_kostka_med
+    else:
+        price += size * value_kostka_big
+    price = ceiling_half(price)
+    return price
+
+def kostka_tr(szer, dl, wys, tkan, wata):
+    size = szer*dl + szer*wys + wys*dl + math.sqrt(dl*dl+szer*szer)
+    price = mats_price(size, tkan, wata)
+    if size < 3700:
+        price += size * value_kostka_tr_small
+    else:
+        price += size * value_kostka_tr_med
+    price = ceiling_half(price)
+    return price
+
+def swinka(szer, dl, wys, tkan, wata):
+    size = szer*dl + (szer*wys + wys*dl)*2
+    price = mats_price(size, tkan, wata)
+    if size < 3500:
+        price += size * value_swinka_small
+    else:
+        price += size * value_swinka_big
+    price = ceiling_half(price)
+    return price
+
+
+def hamak(szer, dl, tkan, wata):
+    size = szer*dl
+    price = mats_price(size, tkan, wata)
+    if size < 1000:
+        price += size * value_hamak_mini
+    else:
+        price += size * value_hamak
+    price = ceiling_half(price)
+    return price
---
+++ prices.py
@@ -0,0 +1,21 @@
+PRICE_min = 0.00192308
+PRICE_pol = 0.00078125
+PRICE_baw = 0.00059375
+PRICE_wat = 0.00037500
+PRICE_pik = 0.00056250
+
+value_waste = 0.0003
+
+value_hamak = 0.004
+value_hamak_mini = 0.005
+
+value_kostka_small = 0.0073
+value_kostka_med = 0.006
+value_kostka_big = 0.0037
+
+value_kostka_tr_small = 0.0085
+value_kostka_tr_med = 0.0068
+
+
+value_swinka_small = value_hamak
+value_swinka_big = value_kostka_big
