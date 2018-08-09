a890c66f2d63e2794d516f39ee02f028de24bb87
Frogkiller <frogkillerpl@gmail.com>
2018-08-09 20:38:03 +0200

    added missing math import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import math
+
 from prices import *
 import backend
 
@@ -23,8 +23,8 @@ def generator(tata, lista):
         lab.pack(side=tk.LEFT)
         entr = tk.Entry(row)
         entr.pack(side=tk.RIGHT)
-        row.pack(side=tk.TOP, expand = True, fill = tk.BOTH)
-        enti[x] =  entr
+        row.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+        enti[x] = entr
     return enti
 
 
@@ -42,7 +42,7 @@ def ui_maker(where, what):
     where.val = tk.StringVar()
     combo = ttk.Combobox(frame, textvariable=where.val, values=where.mats, state="readonly")
     combo.pack(side=tk.RIGHT)
-    b = tk.Button(where, text="Licz", command=lambda: where.fun())
+    b = tk.Button(where, text="Licz", command=lambda: where.calculate())
     b.pack(anchor=tk.S)
     where.wynik = tk.Entry(where)
     where.wynik.pack(anchor=tk.S)
@@ -53,7 +53,7 @@ class Ham(tk.Frame):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość"])
 
-    def fun(self):
+    def calculate(self):
         self.wynik.delete(0, tk.END)
         wat = self.watolina.get()
         szer = self.itenz[self.entr[0]]
@@ -63,12 +63,13 @@ class Ham(tk.Frame):
         calculations = backend.hamak(int(szer.get()), int(dlug.get()), matpric, wat)
         self.wynik.insert(0, calculations)
 
-class Ham_tr(tk.Frame):
+
+class HamTriangle(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość"])
 
-    def fun(self):
+    def calculate(self):
         self.wynik.delete(0, tk.END)
         wat = self.watolina.get()
         szer = self.itenz[self.entr[0]]
@@ -79,12 +80,13 @@ class Ham_tr(tk.Frame):
         calculations = backend.ceiling_half(hamak_v/2)
         self.wynik.insert(0, calculations)
 
-class Ham_2lvl(tk.Frame):
+
+class Ham2lvl(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość"])
 
-    def fun(self):
+    def calculate(self):
         self.wynik.delete(0, tk.END)
         wat = self.watolina.get()
         szer = self.itenz[self.entr[0]]
@@ -95,12 +97,13 @@ class Ham_2lvl(tk.Frame):
         calculations = 2*hamak_v+2
         self.wynik.insert(0, calculations)
 
-class Kost(tk.Frame):
+
+class Cube(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość", "Wysokość"])
 
-    def fun(self):
+    def calculate(self):
         self.wynik.delete(0, tk.END)
         wat = self.watolina.get()
         szer = self.itenz[self.entr[0]]
@@ -111,7 +114,8 @@ class Kost(tk.Frame):
         calculations = backend.kostka(int(szer.get()), int(dlug.get()), int(wys.get()), matpric, wat)
         self.wynik.insert(0, calculations)
 
-class Kost_tr(tk.Frame):
+
+class CubeTriangle(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość", "Wysokość"])
@@ -127,12 +131,13 @@ class Kost_tr(tk.Frame):
         calculations = backend.kostka_tr(int(szer.get()), int(dlug.get()), int(wys.get()), matpric, wat)
         self.wynik.insert(0, calculations)
 
-class Swin(tk.Frame):
+
+class Pig(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         ui_maker(self, ["Szerokość", "Długość", "Wysokość"])
 
-    def fun(self):
+    def calculate(self):
         self.wynik.delete(0, tk.END)
         wat = self.watolina.get()
         szer = self.itenz[self.entr[0]]
@@ -144,12 +149,12 @@ class Swin(tk.Frame):
         self.wynik.insert(0, calculations)
 
 
-class app():
-    def __init__(self, root):
+class Calculator:
+    def __init__(self, rt):
         self.frame_ = None
-        self.root = root
+        self.root = rt
 
-    def switchFrame(self, what):
+    def switch_frame(self, what):
         if self.frame_ is not None:
             self.frame_.destroy()
         self.frame_ = what(self.root)
@@ -166,18 +171,19 @@ if __name__ == '__main__':
     separator.pack(fill=tk.X, padx=5, pady=5)
     frame2 = tk.Frame(root)
     frame2.pack()
-    AP = app(frame2)
+    AP = Calculator(frame2)
     v = tk.IntVar()
-    tk.Radiobutton(frame1, text="Hamak", variable=v, value=1, command=lambda: AP.switchFrame(Ham)).pack(side=tk.LEFT)
-    tk.Radiobutton(frame1, text="Trójkątny hamak", variable=v, value=2, command=lambda: AP.switchFrame(Ham_tr)).pack(
+    tk.Radiobutton(frame1, text="Hamak", variable=v, value=1, command=lambda: AP.switch_frame(Ham)).pack(side=tk.LEFT)
+    tk.Radiobutton(frame1, text="Trójkątny hamak", variable=v, value=2,
+                   command=lambda: AP.switch_frame(HamTriangle)).pack(side=tk.LEFT)
+    tk.Radiobutton(frame1, text="Domek kostka", variable=v, value=3, command=lambda: AP.switch_frame(Cube)).pack(
         side=tk.LEFT)
-    tk.Radiobutton(frame1, text="Domek kostka", variable=v, value=3, command=lambda: AP.switchFrame(Kost)).pack(
+    tk.Radiobutton(frame1, text="Domek narożnik", variable=v, value=4,
+                   command=lambda: AP.switch_frame(CubeTriangle)).pack(
         side=tk.LEFT)
-    tk.Radiobutton(frame1, text="Domek narożnik", variable=v, value=4, command=lambda: AP.switchFrame(Kost_tr)).pack(
-        side=tk.LEFT)
-    tk.Radiobutton(frame1, text="Domek świnka", variable=v, value=5, command=lambda: AP.switchFrame(Swin)).pack(
+    tk.Radiobutton(frame1, text="Domek świnka", variable=v, value=5, command=lambda: AP.switch_frame(Pig)).pack(
         side=tk.LEFT)
     tk.Radiobutton(frame1, text="Paśnik/ham z dziurka/plaster 1 poziomowy", variable=v, value=6,
-                   command = lambda: AP.switchFrame(Ham_2lvl)).pack(side=tk.LEFT)
+                   command=lambda: AP.switch_frame(Ham2lvl)).pack(side=tk.LEFT)
 
     root.mainloop()
